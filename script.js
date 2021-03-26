844cd5df5fa81d79e28d10bcad03ef64c7b5394c
Alexey Mitsenko <alex.micenko@gmail.com>
2021-03-26 12:12:00 +0300

    some fixes

--- script.js
+++ script.js
@@ -1,44 +1,46 @@
 const quizData = [{
-    question: "If soccer is called football in England, what is American football called in England?",
-    a: "American football",
-    b: "Combball",
-    c: "Handball",
-    d: "Touchdown",
-    correct: "a"
+    "question": "If soccer is called football in England, what is American football called in England?",
+    "a": "American football",
+    "b": "Combball",
+    "c": "Handball",
+    "d": "Touchdown",
+    "correct": "a"
 },
 {
-    question: "What is the largest country in the world?",
-    a: "Russia",
-    b: "Canada",
-    c: "China",
-    d: "United States",
-    correct: "a"
+    "question": "What is the largest country in the world?",
+    "a": "Russia",
+    "b": "Canada",
+    "c": "China",
+    "d": "United States",
+    "correct": "a"
 },
 {
-    question: "An organic compound is considered an alcohol if it has what functional group?",
-    a: "Hydroxyl",
-    b: "Carbonyl",
-    c: "Alkyl",
-    d: "Aldehyde",
-    correct: "a"
+    "question": "An organic compound is considered an alcohol if it has what functional group?",
+    "a": "Hydroxyl",
+    "b": "Carbonyl",
+    "c": "Alkyl",
+    "d": "Aldehyde",
+    "correct": "a"
 },
 {
-    question: "What is the 100th digit of Pi?",
-    a: "9",
-    b: "4",
-    c: "7",
-    d: "2",
-    correct: "a"
+    "question": "What is the 100th digit of Pi?",
+    "a": "9",
+    "b": "4",
+    "c": "7",
+    "d": "2",
+    "correct": "a"
 },
 {
-    question: "A doctor with a PhD is a doctor of what?",
-    a: "Philosophy",
-    b: "Psychology",
-    c: "Phrenology",
-    d: "Physical Therapy",
-    correct: "a"
+    "question": "A doctor with a PhD is a doctor of what?",
+    "a": "Philosophy",
+    "b": "Psychology",
+    "c": "Phrenology",
+    "d": "Physical Therapy",
+    "correct": "a"
 }
-]
+];
+
+
 let currentQuest = 0;
 let currentQuiz = 0;
 let score = 0;
@@ -52,10 +54,15 @@ const b_text = document.getElementById("b_text");
 const c_text = document.getElementById("c_text");
 const d_text = document.getElementById("d_text");
 const answerElems = document.getElementsByName('answer');
+const mainContElem = document.getElementById('main-container');
+
+
+const answerKeys = ["a", "b", "c", "d"];
+
 
 let yourAnswers = [];
 let correctAnswers = [];
-for (let i=0;i<quizData.length;i++){
+for (let i = 0; i < quizData.length; i++) {
     correctAnswers.push(quizData[i].correct);
 }
 
@@ -79,7 +86,6 @@ function getSelectedItem() {
 
     let answer = undefined;
     answerElems.forEach((answerElem) => {
-        // console.log(answerElem.checked);
         if (answerElem.checked) {
             answer = answerElem.id;
             yourAnswers.push(answerElem.id);
@@ -89,11 +95,55 @@ function getSelectedItem() {
     return answer;
 }
 
+function _LiGenerator() {
+    let answerElement = document.createElement("li");
+    let inputElement = document.createElement("input");
+    inputElement.setAttribute("type", "radio");
+    inputElement.setAttribute("disabled", "true");
+    inputElement.setAttribute("id", "a");
+    inputElement.setAttribute("name", "answer");
+    inputElement.setAttribute("class", "answer");
+    let labelElement = document.createElement("label");
+
+    
+
+    labelElement.setAttribute("id", "a_text");
+    labelElement.setAttribute("for", "a");
+    labelElement.innerHTML = "Question";
+    answerElement.appendChild(inputElement);
+    answerElement.appendChild(labelElement);
+    answersList.appendChild(answerElement);
+}
+
+
+function showResults() {
+    for (let i = quizData.length - 2; i >= 0; i--) {
+        let questionHeader = document.createElement("h2");
+        questionHeader.innerHTML = quizData[i].question;
+        questionHeader.setAttribute("id", "question");
+        let quizContainerDiv = document.createElement("div");
+        quizContainerDiv.setAttribute("class", "quiz-container");
+        let answersList = document.createElement("ul");
+
+        var oCurrentQuizData = quizData[i];
+        answerKeys.reduce(function (sAcc, sKey) {
+            var sDomNodeLi = 
+        }.bind(this), "")
+
+        for (let i = 0; i < quizData.length - 1; i++) {
+
+        }
+
+        mainContElem.appendChild(questionHeader);
+        mainContElem.appendChild(quizContainerDiv);
+        quizContainerDiv.appendChild(answersList);
+    }
+}
+
 sbmtButt.addEventListener('click', () => {
 
     const answer = getSelectedItem();
     if (answer) {
-        console.log(answer);
         if (answer == quizData[currentQuiz].correct)
             score++;
         currentQuiz++;
@@ -109,6 +159,16 @@ sbmtButt.addEventListener('click', () => {
             for (i = 0; i < answerElems.length; i++) {
                 answerElems[i].checked = false;
             }
+            //ToDo after quiz
+            sbmtButt.parentNode.removeChild(sbmtButt);
+            for (let i = 0; i < answerElems.length; i++)
+                answerElems[i].disabled = true;
+
+            showResults();
+            // yourAnswers = [];
+            // currentQuest = 0;
+            // currentQuiz = 0;
+            // loadQuiz();
         }
     }
 });
