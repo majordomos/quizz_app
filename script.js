eda9c63fef86a58e654485903cf20fa024793125
Alexey Mitsenko <alex.micenko@gmail.com>
2021-03-26 09:01:41 +0300

    ready design

---
+++ script.js
@@ -0,0 +1,42 @@
+const quizData = [{
+    question: "2+2=?",
+    a:"1",
+    b:"2",
+    c:"3",
+    d:"4",
+    correct:"d"
+},
+{
+    question: "3*6=?",
+    a:"15",
+    b:"14",
+    c:"18",
+    d:"22",
+    correct:"c"
+},
+{
+    question: "Capital of Belarus",
+    a:"Minsk",
+    b:"Grodno",
+    c:"Gomel",
+    d:"New York",
+    correct:"a"
+},
+{
+    question: "22-15=?",
+    a:"9",
+    b:"7",
+    c:"8",
+    d:"123",
+    correct:"b"
+},
+{
+    question: "2%2=?",
+    a:"0",
+    b:"2",
+    c:"1",
+    d:"3",
+    correct:"0"
+},
+]
+
